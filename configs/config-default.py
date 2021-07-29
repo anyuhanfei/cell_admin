8ada96060b8c5b6cca6133751c37d1c6646d73dd
暗语寒飞 <1223050251@qq.com>
2021-07-29 13:31:59 +0800

    添加默认配置文件

---
+++ configs/config-default.py
@@ -0,0 +1,31 @@
+APP_DEBUG = True
+
+# power
+DEVELOPER = True
+ADMIN_POWER = False
+
+# Database
+DATABASE = {
+    'TYPE': 'mysql',
+    'HOSTNAME': '127.0.0.1',
+    'DATABASE': 'cell_admin',
+    'USERNAME': 'root',
+    'PASSWORD': 'root',
+    'HOSTPORT': '3306'
+}
+
+# User
+USER = {
+    'USER_IDENTITY_TEXT': '手机号',  # 会员标识说明
+    'USER_FUND_TYPE': {  # 账户币种, key(币种名): value(币种名中文说明)
+        'money': '余额',
+        'integral': '积分',
+        'gold': '金币',
+        'usdt': 'USDT'
+    },
+    'VIO_ENABLE': True,
+    'USER_DATA_KEYS': [
+        'vio', '标签', 'phone', 'email'
+    ],
+    'USER_DELETE': False,  # 会员删除是否使用
+}
